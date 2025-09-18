f60e8369051e4e619d1a8659ec063f269d2b1a4e
Fujiwara <83638372+AZRAELSANTI@users.noreply.github.com>
2025-09-19 00:37:24 +0300

    Добавьте файлы проекта.

---
+++ AR_App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AR_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
