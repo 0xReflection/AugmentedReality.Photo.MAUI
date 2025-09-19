ec1629b41747347e6dbc43ac1de0446a5fb4c27a
Reflection <desiderata_cpp@icloud.com>
2025-09-19 12:39:41 +0300

    1 наметки кринжа

--- Presentation/AppShell.xaml.cs
+++ Presentation/AppShell.xaml.cs
@@ -6,5 +6,9 @@
         {
             InitializeComponent();
         }
+        //this.HomeTab.Icon = ImageSource.FromResource("Presentation.Resources.Images.home.png", this.GetType().Assembly);
+        //this.SectionsTab.Icon = ImageSource.FromResource("Presentation.Resources.Images.categories.png", this.GetType().Assembly);
+        //this.BookmarksTab.Icon = ImageSource.FromResource("Presentation.Resources.Images.bookmarks.png", this.GetType().Assembly);
+        //this.ProfileTab.Icon = ImageSource.FromResource("Presentation.Resources.Images.profile.png", this.GetType().Assembly);
     }
 }
---
+++ Presentation/MauiProgram.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using Presentation.ViewModel;
+
+namespace Presentation
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                
+                
+                .UseMauiApp<App>()
+                .RegisterViewModels()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("NotoSerif-Bold.ttf", "NotoSerifBold");
+                    fonts.AddFont("Poppins-Bold.ttf", "PoppinsBold");
+                    fonts.AddFont("Poppins-SemiBold.ttf", "PoppinsSemibold");
+                    fonts.AddFont("Poppins-Regular.ttf", "Poppins");
+                    fonts.AddFont("MaterialIconsOutlined-Regular.otf", "Material");
+
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+        public static MauiAppBuilder RegisterViewModels(this MauiAppBuilder mauiAppBuilder)
+        {
+            mauiAppBuilder.Services.AddTransient<MainViewModel>();
+            //mauiAppBuilder.Services.AddTransient<SectionsViewModel>();
+            //mauiAppBuilder.Services.AddTransient<ArticleViewModel>();
+            //mauiAppBuilder.Services.AddTransient<BookmarksViewModel>();
+
+            mauiAppBuilder.Services.AddTransient<MainPage>();
+            //mauiAppBuilder.Services.AddTransient<SectionsPage>();
+            //mauiAppBuilder.Services.AddTransient<ArticlePage>();
+            //mauiAppBuilder.Services.AddTransient<BookmarksPage>();
+
+            return mauiAppBuilder;
+        }
+    }
+}
